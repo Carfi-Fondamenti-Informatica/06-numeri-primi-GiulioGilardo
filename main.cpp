d9cf9c07b9c3457d02e57aad98a64d50ce530465
GiulioGilardo <116483336+GiulioGilardo@users.noreply.github.com>
2022-12-03 11:21:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
 
-int main(){
-  return 0;
+    int a;
+    cin>>a;
+    if(nprimo(a) == true){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
+    return 0;
 }
